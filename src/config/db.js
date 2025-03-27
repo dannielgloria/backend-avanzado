d97301ddbea2f693c266fecd2bd8cd4cec22080b
dannielgloria <danniel.gloria@gmail.com>
2025-03-26 19:55:02 -0600

    feature: Conexión a MongoDB

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://dannielgloria:iySbmxO7sZFox4f9@ejerciciokata.74apu.mongodb.net/', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
