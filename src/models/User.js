01a414cc523fa3d6f33f898ed8ea96029a5e18da
dannielgloria <danniel.gloria@gmail.com>
2025-03-31 20:26:24 -0600

    feature: Se atributo de pasword al modelo User y se agrega un metodo de hasheo

--- src/models/User.js
+++ src/models/User.js
@@ -12,8 +12,20 @@ const UserSchema = new mongoose.Schema({
         require: true,
         unique: true,
         match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+        type: String,
+        required: true,
     }
+
 });
+
+// Método para hasear el password antes de guardar el usuario
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
 
 const User = mongoose.model('User', UserSchema);
 export default User;
