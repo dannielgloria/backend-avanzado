cc807d7f868c6807c268796de37542fc28bcde1c
dannielgloria <danniel.gloria@gmail.com>
2025-03-31 20:59:36 -0600

    feature: Se agrega logica de authentification y hashing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import connectDB from "./src/config/db.js";
 import healtcheckRoutes from "./src/routes/healtcheckRoutes.js"
 import userRoutes from "./src/routes/userRoutes.js"
+import authRoutes from "./src/routes/authRoutes.js"
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ const app = express()
 app.use(express.json());
 app.use('/api/v0/', healtcheckRoutes)
 app.use('/api/v0/users', userRoutes)
+app.use('/api/v0/auth', authRoutes)
 
 
 const PORT = 5010;
---
+++ src/controllers/authController.js
@@ -0,0 +1,49 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+
+
+const generateToken = (id) => {
+    return jwt.sign({id}, "u6ro86beo86re", { expiresIn: '30d'})
+}
+
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExist = await User.findOne({email});
+
+    if (userExist) {
+        res.status(400).json({ message: 'User already exists' });
+        return;
+    }
+
+    const user = await User.create({name, email, password});
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400).json({ message: 'Invalid User data' });
+    }
+}
+
+
+export const authenticateUser = async (req, res) => {
+    const { email, password } = req.body;
+    const userExist = await User.findOne({email});
+
+    if (userExist && (await bcrypt.compare(password, userExist.password))) {
+        res.json({
+            _id: userExist._id,
+            name: userExist.name,
+            email: userExist.email,
+            token: generateToken(userExist._id)
+        })
+    }else{
+        res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+}
