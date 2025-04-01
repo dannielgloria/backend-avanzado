706fcdfb4291f6d7ffcc383e88bea9ce74c491f9
dannielgloria <danniel.gloria@gmail.com>
2025-03-31 19:42:23 -0600

    feature: Se crea el modelo y controlador de Post, como ejemplo de relacion user-post

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import Post from "../models/Post.js";
 
 export const createUser = async (req, res) => {
     try {
@@ -20,3 +21,37 @@ export const getAllUsers = async (req, res) => {
         res.status(400).json({message: error.message})
     }
 }
+
+export const getPost = async (req, res) => {
+    try {
+        const posts = await Post.find().populate('user', 'name email');
+        res.status(201).json(posts)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const createPost = async (req, res) =>{
+    const { title, userId } = req.body;
+
+    // Verificamos si el usuario existe en Mongo
+    const user = await User.findById(userId);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found'});
+    }
+
+    //Creamos Post
+    const post = new Post({
+        title,
+        user: userId
+    });
+
+    // Aqui se guardara nuestro objeto post en Mongodb
+    try {
+        await post.save()
+        res.status(201).json(post)
+    } catch (error) {
+        return res.status(400).json({ message: error.message});
+    }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createUser, getAllUsers } from '../controllers/userController.js';
+import { createUser, getAllUsers, getPost, createPost } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.route('/create').post(createUser).get(getAllUsers)
+router.route('/post').post(createPost).get(getPost)
 
 export default router
