d581b204c77aa04e6e327aadfaeac12fea082bf8
dannielgloria <danniel.gloria@gmail.com>
2025-04-02 21:16:35 -0600

    feature: Se crea validacion de middleware a post

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,11 @@ import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
 import { createUser, getAllUsers, getPost, createPost } from '../controllers/userController.js';
 import { userValidation } from '../middleware/userValidationMiddleware.js'
+import { postValidation } from '../middleware/postValidationMiddleware.js';
 
 const router = express.Router();
 
 router.route('/create').post(userValidation, createUser).get(getAllUsers)
-router.route('/post').post(protect, createPost).get(getPost)
+router.route('/post').post(protect,postValidation, createPost).get(getPost)
 
 export default router
