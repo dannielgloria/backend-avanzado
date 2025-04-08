5003779961c7af0a326944738eb91490d12e9d12
dannielgloria <danniel.gloria@gmail.com>
2025-04-07 20:02:02 -0600

    feature: Se agrega libreria admin y se agrega tipo de importacion .json a conexion de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 import { firebaseStorage, googleCredentials } from './constants.js'
 
-import serviceAccount from '../../backend-avanzado-cac5a-firebase-adminsdk-fbsvc-5cf2653750.json'
+import serviceAccount from '../../backend-avanzado-cac5a-firebase-adminsdk-fbsvc-5cf2653750.json' with { type: "json" };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
