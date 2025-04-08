90fbb36713c922773a2d1fd4064cebdc94ecba56
dannielgloria <danniel.gloria@gmail.com>
2025-04-07 21:30:07 -0600

    test: se genera codigo para pruebas unitarias

--- test/integration/authController.test.js
+++ test/integration/authController.test.js
@@ -6,11 +6,13 @@ import User from "../../src/models/User";
 
 let mongoServer;
 
-beforeAll( async () => {
+beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri()
+    const mongoUri = mongoServer.getUri();
 
-    await mongoose.connect(mongoUri)
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(mongoUri);
+    }
 });
 
 afterAll( async () => {
@@ -23,7 +25,7 @@ afterEach( async () => {
 })
 
 describe('Prueba de integracion - AuthController.js', () => {
-    
+
     test('Registro de Usuario Exitoso', async () => { 
         const response = await request(app)
             .post('/api/v0/auth/register')
@@ -36,8 +38,26 @@ describe('Prueba de integracion - AuthController.js', () => {
         expect(response.statusCode).toBe(201);
         expect(response.body).toHaveProperty('_id');
         expect(response.body).toHaveProperty('token');
-        expect(response.body.email).toHaveProperty('juan@example.com');
-        
-     })
+        expect(response.body.email).toBe('juan@example.com');
+    });
+
+    test('Falla al registrar usuario ya existente', async () => {
+        await User.create({
+          name: 'Juan Perez',
+          email: 'juan@example.com',
+          password: 'hashedPassword123',
+        });
+    
+        const response = await request(app)
+          .post('/api/v0/auth/register')
+          .send({
+            name: 'Juan Perez',
+            email: 'juan@example.com',
+            password: 'secure123',
+          });
+    
+        expect(response.statusCode).toBe(400);
+        expect(response.body.message).toBe('User already exists');
+      });
 
 });
