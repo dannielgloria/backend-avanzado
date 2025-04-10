f1543d35aafa9156140699d42925be0cbe08af1a
dannielgloria <danniel.gloria@gmail.com>
2025-04-09 19:52:11 -0600

    feat: se agregan cambios oara desplegar en netlify

--- src/middleware/uploadLocalMiddleware.mjs
+++ src/middleware/uploadLocalMiddleware.mjs
@@ -2,8 +2,8 @@ import multer from "multer";
 import path from 'path';
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __nombrearchivo = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__nombrearchivo)
 
 // Definimos almacenamiento local
 const storage = multer.diskStorage({
